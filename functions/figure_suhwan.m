d5ac06cda23d06367cc487576da16d5bd983ea8a
Suhwan Gim <roseno.9@daum.net>
2019-03-23 17:24:46 +0900

    functions

--- functions/figure_suhwan.m
+++ functions/figure_suhwan.m
@@ -1,16 +1,41 @@
-function figure_suhwan(varargin)
-
-fn = 1;
-for i = 1:length(varargin)   
+function figure_Handle = figure_suhwan(varargin)
+%%
+% create figure with white backgroud and bigger size.
+%
+% :: Usage
+%   figure_Handle = figure_suhwan(n);
+%
+% :: Optional input
+%       1. n: figure number
+% 
+% :: Output (it works from R2014b)
+%       1. figure_Handle: figure handle        
+% 
+% :: Examples
+%       figure_suhwan(2);
+%       figure_suhwan;
+%
+% Written by Suhwan Gim (19. 03. 22) 
+%%
+fn = 1; %figure number
+%title_str=[];
+for i = 1:length(varargin)
     if isnumeric(varargin{i})
         fn = varargin{i};
-    else
-        warning('Invalid input for figure index (not numeric value)');
-        fn = 1;
     end
-end
+    
+    if ischar(varargin{i})
+        switch varargin{i}
+            % functional commands
+            case {'title'}
+            %    title_str = varargin{i+1};
+        end
+    end
+    
 
-figure(fn)
+end
+%%
+figure_Handle = figure(fn);
 set(gcf, 'Position', [-1919        -279        1920         984]);
 set(gcf, 'color', 'w');
 
--- functions/plot_suhwan.m
+++ functions/plot_suhwan.m
@@ -1,7 +1,22 @@
-function h=plot_suhwan(x,y,varargin)
+function h = plot_suhwan(x,y,varargin)
 %%
-%asdfasdfa
-
+% create plot with white backgroud and bigger size
+%
+% :: Usage
+%       plot_suhwan(x,y,'color',[0,0,1],'linwidth',2);
+%
+% :: Optional input
+%       1. color
+%       2. linewidth
+%
+% :: Output (it works from R2014b)
+%       1. plot: plot axis handle       
+% 
+% :: Examples
+%       h = plot_suhwan(x,y,'color',[0,0,1],'linwidth',2);
+%       plot_suhwan(x,y);
+%
+% Written by Suhwan Gim (19. 03. 22) 
 %%
 % Parse varargin
 color = [0.3333, 0.6588, 1.0000];
@@ -22,7 +37,7 @@ facecolor = [0.2 0.2 0.35;
     0.5 0.5 1;
     1 0.5 0.5;
     1 0.7333 1];
-figure
+figure;
 set(gcf, 'Position', [-1919        -279        1920         984]);
 set(gcf, 'color', 'w');
 
@@ -30,6 +45,6 @@ h = plot(x,y,'-','color',color','linewidth',linew);
 xlabel('Time point', 'fontsize', 22);
 ylabel('Effect magnitude', 'fontsize', 22);
 
-set(gca,'FontSize',22);
+set(gca, 'FontSize',22);
 set(gca, 'linewidth', 1.5);%,'xlim',[0 N+0.5],'XTick',[1,N],'XTickLabel', [0,14.5],'ylim',[-0.15 1.5]);% ,'XTickLabelRotation',360-45);
 end
