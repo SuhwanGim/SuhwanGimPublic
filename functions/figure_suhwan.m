d539573cf9bf5d8125a6aea93f83f1b58dfbca3a
SuhwanGim <roseno.9@daum.net>
2019-04-09 18:49:12 +0900

    added some functional commands: figure_suhwan.m

--- functions/figure_suhwan.m
+++ functions/figure_suhwan.m
@@ -18,17 +18,22 @@ function figure_Handle = figure_suhwan(varargin)
 % Written by Suhwan Gim (19. 03. 22) 
 %%
 fn = 1; %figure number
+sz = [-1919        -279        1920         984];
 %title_str=[];
+
 for i = 1:length(varargin)
-    if isnumeric(varargin{i})
-        fn = varargin{i};
-    end
+%     if isnumeric(varargin{i})
+%         fn = varargin{i};
+%     end
     
     if ischar(varargin{i})
-        switch varargin{i}
+        switch lower(varargin{i})
             % functional commands
-            case {'title'}
+            case {'number'}
+                fn = varargin{i+1};
             %    title_str = varargin{i+1};
+            case {'size','position'}
+                sz = varargin{i+1};
         end
     end
     
@@ -36,7 +41,7 @@ for i = 1:length(varargin)
 end
 %%
 figure_Handle = figure(fn);
-set(gcf, 'Position', [-1919        -279        1920         984]);
+set(gcf, 'Position', sz);
 set(gcf, 'color', 'w');
 
 end
